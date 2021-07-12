e5aacb2cb638b24c9eb9f68ef760c24ade9e77cf
Swamy <swamyvinny@gmail.com>
2021-07-12 11:33:52 -0700

    hopefully fixed bug with missing data, outputing in long format

--- parse_sample_counts/src/main.rs
+++ parse_sample_counts/src/main.rs
@@ -3,16 +3,16 @@ use std::io::{ BufRead, BufWriter, Write, stdout, stdin};
 use std::collections::HashMap;
 //use regex::Regex;
 //use lazy_static::lazy_static;
-use itertools::Itertools;
+//use itertools::Itertools;
 fn main() {
     //let sample_counts_file = fs::File::open(stdin()).expect("bad input");
     //let outfile = fs::File::create("parsed_counts.tsv").unwrap();
     let mut  writer = BufWriter::new(stdout());
-    let snaptron_ids = fs::read_to_string("/data/swamyvs/reviz/ids.txt").unwrap();
-    let snaptron_ids = snaptron_ids.split("\n").collect::<Vec<&str>>();
+    let sample_ids = fs::read_to_string("/data/swamyvs/reviz/ids.txt").unwrap();
+    let sample_ids = sample_ids.split("\n").collect::<Vec<&str>>();
     let mut id_mapper:HashMap<&str, usize> = HashMap::new();
-    for i in 0..snaptron_ids.len() {
-        id_mapper.insert(&snaptron_ids[i], i);
+    for i in 0..sample_ids.len() {
+        id_mapper.insert(&sample_ids[i], i);
     }
     let stdin = stdin();
     let reader = stdin.lock();
@@ -25,17 +25,26 @@ fn main() {
 
 
 fn parse_line(line: String, mapper: &HashMap<&str, usize>) -> String{
-    // lazy_static! {
-    //     static ref sample_pat: Regex = Regex::new(r"^\d+").unwrap();
-    //     static ref count_pat: Regex = Regex::new(r"\d+$").unwrap();
-    // }
-    let line_vec =  line.trim_matches(',').split(',').collect::<Vec<&str>>();
-    let sample_counts = line_vec.iter().map(|x| x.split(':').collect_tuple().unwrap()).collect::<Vec<(&str, &str)>>();
-    let mut empty_vec = vec!["0"; mapper.len()];
-    for sample in sample_counts{
-        let idx = mapper.get(sample.0).expect("sample id missing from railid_list");
-        empty_vec[*idx] = sample.1;
+    let line_data_raw:Vec<&str> = line.split('|').collect();
+    let snaptron_id = line_data_raw[0];
+    let msg = String::from("N");
+    //line_data_raw[1] = sample_counts
+    let line_vec = line_data_raw[1].trim_matches(',').split(',').collect::<Vec<&str>>();
+    let mut empty_vec = vec![msg; mapper.len()];
+    for count_raw in line_vec{
+        let count_split = count_raw.split(':').collect::<Vec<&str>>();
+        if count_split.len() < 2{
+            continue;
+        } else if count_split.len() > 2{
+            panic!("a record had multiple values, we don't know how to handle this");
+        } else{    
+            let idx = mapper.get(count_split[0]).expect("Sample id missing from rail_id metadata");
+            let formatted_counts = format!("{},{}", snaptron_id, count_split[1]);
+            empty_vec[*idx] = formatted_counts; 
+        }
     }
-    let clean_string = empty_vec.join(",");
+    let msg = String::from("N");
+    empty_vec.retain(|x| x != &msg);
+    let clean_string = empty_vec.join("\n");
     return clean_string;
 }
